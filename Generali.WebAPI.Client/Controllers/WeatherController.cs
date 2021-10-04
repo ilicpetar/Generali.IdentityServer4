aa5cdbf2451c4ca9aa7eec97a9e05ec8140b4267
ilicpetar <ilic.petar@gmail.com>
2021-10-04 17:18:16 +0200

    Add project files.

---
+++ Generali.WebAPI.Client/Controllers/WeatherController.cs
@@ -0,0 +1,45 @@
+﻿using Generali.WebAPI.Client.Models;
+using Generali.WebAPI.Client.Services;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Generali.WebAPI.Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private IIdentityServer4Service _identityServer4Service = null;
+        public WeatherController(IIdentityServer4Service identityServer4Service)
+        {
+            _identityServer4Service = identityServer4Service;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var OAuth2Token = await _identityServer4Service.GetToken("weatherApi.read");
+            using (var client = new HttpClient())
+            {
+                client.SetBearerToken(OAuth2Token.AccessToken);
+                var result = await client.GetAsync("https://localhost:44325/weatherforecast");
+                if (result.IsSuccessStatusCode)
+                {
+                    var model = await result.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<WeatherForecast>>(model);
+                }
+                else
+                {
+                    throw new Exception("Some Error while fetching Data");
+                }
+            }
+        }
+    }
+}
